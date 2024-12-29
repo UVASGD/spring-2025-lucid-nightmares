0bac62d991269727f1fb10801ba1d4040d95c63b
Arti-Dev <boomcreeper296@gmail.com>
2024-12-28 19:03:16 -0500

    Change Free Tile to CharacterBody2D

--- scripts/free_tile.gd
+++ scripts/free_tile.gd
@@ -1,16 +1,26 @@
 extends TelekineticObject
 
-@onready var animBody = $AnimatableBody2D
-@onready var marker = $Marker
+@onready var charBody = $CharacterBody2D
+@onready var marker = $CharacterBody2D/Marker
+
+var maxSpeed = 300
+var friction = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	marker.visible = false
+	marker.visible = true
+	is_selected = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if is_selected:
+		# calculate direction to go
+		var x = Input.get_axis("TelekineticLeft", "TelekineticRight")
+		var y = Input.get_axis("TelekineticUp", "TelekineticDown")
+		var dir: Vector2 = Vector2(x, y)
+		charBody.velocity = dir * maxSpeed
+	charBody.move_and_slide()
 
 func set_selected(boo: bool):
 	is_selected = boo
