e8376f4754be331ef1d404e97d78644b3bc9b161
Arti-Dev <boomcreeper296@gmail.com>
2024-12-30 15:44:43 -0500

    Properly copy the scene
    Use the in-editor duplication feature, since if two scenes have the same UID it messes stuff up

--- scripts/moving_platform_path.gd
+++ scripts/moving_platform_path.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
 @onready var animBody = $AnimatableBody2D
-@export var offset = Vector2(0, -200)
-@export var duration = 5.0
-@export var pauseTime = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
