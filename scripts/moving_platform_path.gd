280a3b969ed93bd1f5860acfd6a83debfad09b9e
Arti-Dev <boomcreeper296@gmail.com>
2024-12-30 19:27:03 -0500

    Implement moving platform via pathing

--- scripts/moving_platform_path.gd
+++ scripts/moving_platform_path.gd
@@ -1,12 +1,18 @@
-extends Node2D
+extends Path2D
 
 @onready var animBody = $AnimatableBody2D
 
+# to set up:
+# instantiate this scene
+# define a Curve2D in the inspector of the scene node
+@onready var pathFollow: PathFollow2D = $PathFollow2D
+@export var speed = 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(get_children().size())
-
+	pass
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	pathFollow.progress += speed * delta
