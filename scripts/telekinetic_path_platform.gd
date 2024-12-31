1200a518d5e20b15970637df6ba0e38b06a3bcfc
Arti-Dev <boomcreeper296@gmail.com>
2024-12-30 21:10:11 -0500

    Add telekinetic path platform

---
+++ scripts/telekinetic_path_platform.gd
@@ -0,0 +1,28 @@
+extends TelekineticObject
+
+@onready var animBody = $AnimatableBody2D
+@onready var marker = $AnimatableBody2D/Marker
+
+# to set up:
+# instantiate this scene
+# define a Curve2D in the inspector of the scene node
+@onready var pathFollow: PathFollow2D = $PathFollow2D
+@export var speed = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+		
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if is_selected:
+		var direction = Input.get_axis("TelekineticLeft", "TelekineticRight")
+		pathFollow.progress += speed * delta * direction
+
+func set_selected(boo: bool):
+	is_selected = boo
+	if boo:
+		marker.visible = true
+	else:
+		marker.visible = false
