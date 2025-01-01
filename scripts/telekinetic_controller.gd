5172c4645a4000f126e7b97afc9554015d890186
Arti-Dev <boomcreeper296@gmail.com>
2025-01-01 18:53:02 -0500

    Rename tele object to TelekineticController

--- scripts/telekinetic_controller.gd
+++ scripts/telekinetic_controller.gd
@@ -3,9 +3,15 @@ class_name TelekineticObject
 
 var is_selected: bool = false
 @export var is_enabled: bool = true
+@export var marker: Control = null
 
 func set_enabled(boo: bool):
 	is_enabled = boo
 	
 func set_selected(boo: bool):
 	is_selected = boo
+	if marker != null:
+		if boo:
+			marker.visible = true
+		else:
+			marker.visible = false
