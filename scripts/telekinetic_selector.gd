627dfa1157c999b47436068991d241b053b8f4fc
Arti-Dev <boomcreeper296@gmail.com>
2025-01-01 19:10:56 -0500

    Change selector to work with the new system

--- scripts/telekinetic_selector.gd
+++ scripts/telekinetic_selector.gd
@@ -4,7 +4,7 @@ extends Camera2D
 
 # Queue of 2D bodies
 var queue: Array = []
-var selected_body: Node2D = null
+var selected_node: Node2D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,29 +20,39 @@ func _process(delta: float) -> void:
 		
 
 func _on_telekinetic_area_body_entered(body: Node2D) -> void:
-	var parent = body.get_parent()
+	var teleNode = null
+	for node in body.get_children():
+		if node is TelekineticObject:
+			teleNode = node
+			break
 	# add all objects, even if disabled
-	if parent is TelekineticObject:
-		queue.append(body)
-		if queue.size() == 1 and parent.is_enabled:
-			parent.set_selected(true)
-			selected_body = body
+	if teleNode != null:
+		insertNewNode(teleNode)
+		if queue.size() == 1 and teleNode.is_enabled:
+			teleNode.set_selected(true)
+			selected_node = teleNode
 
 func _on_telekinetic_area_body_exited(body: Node2D) -> void:
-	var parent = body.get_parent()
-	if parent is TelekineticObject:
-		parent.set_selected(false)
-		removeBody(body)
+	# should instead store mappings of bodies to telekinetic nodes - later though
+	var teleNode = null
+	for node in body.get_children():
+		if node is TelekineticObject:
+			teleNode = node
+			break
+
+	if teleNode != null:
+		teleNode.set_selected(false)
+		removeNode(teleNode)
 		
 func cycleQueue(backwards: bool):
-	var enabledQueue: Array = queue.filter(func(body): return body.get_parent().is_enabled)
+	var enabledQueue: Array = queue.filter(func(node): return node.is_enabled)
 	if enabledQueue.size() == 0: return
 	
 	enabledQueue.sort_custom(sortByXGlobalPosition)
 	
-	var index = enabledQueue.find(selected_body)
+	var index = enabledQueue.find(selected_node)
 	if index != -1:
-		selected_body.get_parent().set_selected(false)
+		selected_node.set_selected(false)
 		if backwards: index -= 1
 		else: index += 1
 	else:
@@ -52,22 +62,25 @@ func cycleQueue(backwards: bool):
 	if index == enabledQueue.size(): index = 0
 	if index == -1: index = enabledQueue.size() - 1
 	
-	selected_body = enabledQueue[index]
-	selected_body.get_parent().set_selected(true)
+	selected_node = enabledQueue[index]
+	selected_node.set_selected(true)
 	
-func insertNewBody(body: Node2D):
+func insertNewNode(node: TelekineticObject):
 	# insert into the correct sorted location
+	var body = node.get_parent()
 	for i in range(queue.size()):
-		if queue[i].position.x > body.position.x:
-			queue.insert(i, body)
+		if queue[i].get_parent().position.x > body.position.x:
+			queue.insert(i, node)
 			return
-	queue.append(body)
+	queue.append(node)
 
-func removeBody(body: Node2D):
-	var index = queue.find(body)
+func removeNode(node: TelekineticObject):
+	var index = queue.find(node)
 	if index != -1: queue.remove_at(index)
 	
-func sortByXGlobalPosition(body1: Node2D, body2: Node2D):
+func sortByXGlobalPosition(node1: TelekineticObject, node2: TelekineticObject):
+	var body1 = node1.get_parent()
+	var body2 = node2.get_parent()
 	if body1.global_position.x < body2.global_position.x:
 		return true
 	return false 
