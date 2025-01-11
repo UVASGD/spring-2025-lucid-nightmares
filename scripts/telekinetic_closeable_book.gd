10d823344b57e02e7f7ae16991877a064c10c13f
Arti-Dev <boomcreeper296@gmail.com>
2025-01-10 19:10:43 -0500

    Update texture of closable book

--- scripts/telekinetic_closeable_book.gd
+++ scripts/telekinetic_closeable_book.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var animBody = $AnimatableBody2D
 @onready var teleController: TelekineticController = $AnimatableBody2D/TelekineticController
 @onready var animPlayer: AnimationPlayer = $AnimationPlayer
+@onready var spriteLeft: Sprite2D = $AnimatableBody2D/Sprite2D
+@onready var spriteRight: Sprite2D = $StaticBody2D/Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,3 +15,12 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if teleController.is_selected and Input.is_action_just_pressed("TelekineticRight"):
 		animPlayer.play("fling")
+		
+
+func _on_telekinetic_controller_on_set_enabled(is_enabled: bool) -> void:
+	if is_enabled:
+		spriteLeft.modulate = TelekineticController.spriteModulationColor
+		spriteRight.modulate = TelekineticController.spriteModulationColor
+	else:
+		spriteLeft.modulate = Color("ffffff")
+		spriteRight.modulate = Color("ffffff")
