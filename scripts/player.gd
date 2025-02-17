69bb3b17fdb61f7be2d94776ba9c1277730e31b2
PharingWell <pharingwell@gmail.com>
2025-02-17 18:30:32 -0500

    Squashed commit of the following:
    commit 3226babcab4f93eab4536e041bf82fa55cfd62bb
    Merge: efcf3d8 bbee165
    Author: Arti-Dev <boomcreeper296@gmail.com>
    Date:   Thu Feb 13 00:06:48 2025 -0500
    
        Merge branch 'develop' into movement
    
    commit efcf3d87732e895293165bf47f750304978375f8
    Author: PharingWell <pharingwell@gmail.com>
    Date:   Tue Feb 4 00:26:20 2025 -0500
    
        Added a jump leeway (coyote time) and improved momentum limits
    
    commit d5c878c7b3fd3117a3ccc25f47a154c632c26b18
    Author: PharingWell <pharingwell@gmail.com>
    Date:   Tue Feb 4 00:25:35 2025 -0500
    
        Added better input names and support for W for jump
    
    commit fcaff32cd886898b312073f5e994e1eee873334e
    Author: PharingWell <pharingwell@gmail.com>
    Date:   Tue Feb 4 00:16:35 2025 -0500
    
        Adjusted hitboxes

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,10 +8,13 @@ const GROUND_FRICTION = MAX_SPEED * 0.3
 const AIR_FRICTION = MAX_SPEED * 0.05
 const AIR_CHANGE_SPEED = 10.0
 const JUMP_VELOCITY = -400.0
+const JUMP_LEEWAY_TIME = 0.1
 
 @export var camera: CustomCamera = null
 @onready var remoteTransform: RemoteTransform2D = $RemoteTransform2D
 @onready var animSprite: AnimatedSprite2D = $AnimatedSprite2D
+var canJump: bool = true #for coyote time and adds delay to jump
+var jumpLeewayTimer: float = 0.0
 
 var respawnPosition: Vector2 = Vector2.ZERO
 var checkpoint_phase: int = -1
@@ -22,28 +25,47 @@ func _ready() -> void:
 	respawnPosition = global_position
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	
+	# sets canJump, which determines if the player can jump, ignoring if they are on a platform.
+	# cooldown for jump, and holding down the key looks to weird otherwise
+	# and not being able to hold down the key feels strange
+	if is_on_floor() and not canJump:
+		canJump = true
 
-	# Handle jump.
-	if Input.is_action_just_pressed("PlayerJump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if not is_on_floor():
+		if canJump:
+			#velocity.y = 0 #prevents platform launches
+			# count down remaining "coyote time"
+			jumpLeewayTimer -= delta
+			if jumpLeewayTimer <= 0.0 : 
+				canJump = false
+				jumpLeewayTimer = JUMP_LEEWAY_TIME
+		if not canJump: #if so that jump leeway timer of 0 works properly
+			# Add the gravity.
+			velocity += get_gravity() * delta
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("PlayerLeft", "PlayerRight")
 	if direction:
-		if is_on_floor():
+		if canJump:
 			velocity.x = direction * MAX_SPEED
-		else:
-			velocity.x += direction * AIR_CHANGE_SPEED
-			velocity.x = capSpeed(velocity.x, MAX_SPEED)
+		elif abs(velocity.x) < MAX_SPEED or sign(velocity.x) != sign(direction):
+				velocity.x +=  direction * AIR_CHANGE_SPEED
 	else:
-		if is_on_floor():
+		if canJump:
 			velocity.x = move_toward(velocity.x, 0, GROUND_FRICTION)
 		else:
-			velocity.x = move_toward(velocity.x, 0, AIR_FRICTION)
+			velocity.x = move_toward(velocity.x, 0, AIR_FRICTION)	
+	
+	# handle jump
+	if canJump and Input.is_action_pressed("PlayerJump"):
+		# jumps, even if slightly off platform
+		canJump = false
+		if velocity.y > JUMP_VELOCITY:
+			velocity.y = JUMP_VELOCITY
+	
 	move_and_slide()
 	
 	if direction:
@@ -59,14 +81,6 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ResetToCheckpoint"):
 		respawn()
 
-func capSpeed(speed: float, maximum: float) -> float:
-	if abs(speed) < abs(maximum):
-		return speed
-	else:
-		if speed < 0:
-			return -maximum
-		return maximum
-		
 # This is currently connected to the standing area node and should be connected to another Area2D in the future
 func _on_enter_camera_override_area(area: Area2D) -> void:
 	if area is not CameraOverrideArea: return
