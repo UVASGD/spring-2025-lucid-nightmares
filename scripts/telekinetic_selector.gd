75f74a4703a3c09b11adfd80b2dc4eb2fc2f53d5
Arti-Dev <boomcreeper296@gmail.com>
2025-02-24 20:10:51 -0500

    Implement disconnect SFX

--- scripts/telekinetic_selector.gd
+++ scripts/telekinetic_selector.gd
@@ -3,7 +3,9 @@ class_name TelekineticSelector
 
 @onready var area: Area2D = $TelekineticArea
 @onready var controlLabel: Label = $"../CanvasLayer/TelekineticControlLabel"
+@onready var audioStream = $AudioStreamPlayer2D
 
+var disconnectSound = preload("res://assets/Sounds/Disconnect.wav")
 # Handles selecting telekinetic objects that are in the camera view.
 # To be recognized, a body that comes into frame 
 # must have a TelekineticController object as a direct child.
@@ -71,6 +73,8 @@ func deselectSelectedNode():
 	selected_node.set_selected(false)
 	selected_node = null
 	controlLabel.text = "Tab/Q: Select objects\nR: Reset to last checkpoint"
+	audioStream.stream = disconnectSound
+	audioStream.play()
 	
 func selectNewNode(node: TelekineticController):
 	selected_node = node
