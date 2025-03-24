0f8f051c431bf6d4309b674c00d1f38c765637f7
Arti-Dev <boomcreeper296@gmail.com>
2025-03-24 00:21:00 -0400

    Start on shadow object

---
+++ scripts/shadow.gd
@@ -0,0 +1,63 @@
+extends Polygon2D
+
+enum Direction {UP, DOWN, LEFT, RIGHT}
+
+@export var lightDirection: Direction = Direction.DOWN
+var lightVector: Vector2 = Vector2.ZERO
+@export_category("Define static points")
+## Predefine the points that won't be moving - provide the index of the point on the polygon (e.g. 0 for the 1st point on the polygon)
+@export var staticPoint1: int = -1
+## Predefine the points that won't be moving - provide the index of the point on the polygon (e.g. 0 for the 1st point on the polygon)
+@export var staticPoint2: int = -1
+# array of polygon indices
+var staticPoints: Array[int] = []
+var nonStaticPoints: Array[int] = []
+
+const shadowThickness = 4
+
+@onready var moveShadow = $MovableShadow
+@onready var sprite = $MovableShadow/Sprite2D
+@onready var collisionShape = $MovableShadow/CollisionShape2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	match lightDirection:
+		Direction.UP: lightVector = Vector2(0, -1)
+		Direction.DOWN: lightVector = Vector2(0, 1)
+		Direction.LEFT: lightVector = Vector2(-1, 0)
+		Direction.RIGHT: lightVector = Vector2(1, 0)
+	
+	# Determine which points are static
+	if staticPoint1 == -1 or staticPoint2 == -1:
+		pass
+		return
+	else:
+		staticPoints.append(staticPoint1)
+		staticPoints.append(staticPoint2)
+	for i in range(4):
+		if i not in staticPoints:
+			nonStaticPoints.append(i)
+	
+	# Move the body
+	# midpoint of the two non-static points
+	var movePoint1: Vector2 = polygon[nonStaticPoints[0]]
+	var movePoint2: Vector2 = polygon[nonStaticPoints[1]]
+	moveShadow.position = Vector2((movePoint1.x + movePoint2.x) / 2, (movePoint1.y + movePoint2.y) / 2)
+	
+	# Transform the collision shape using some *magic*
+	var diff: Vector2 = movePoint1 - movePoint2
+	var size = diff * Vector2(lightVector.y, lightVector.x)
+	size = Vector2(abs(size.x), abs(size.y))
+	if size.x == 0:
+		size.x = shadowThickness
+	else:
+		size.y = shadowThickness
+	collisionShape.shape.size = size
+	
+	# Transform the sprite as well
+	sprite.region_rect.size = size
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
