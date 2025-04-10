2eb331ea163548e1159b0f7f5a5883fabf2733dd
Sebastian <pharingwell@gmail.com>
2025-04-10 18:25:29 -0400

    Worked on two train prototypes

--- scripts/telekinetic_path_platform.gd
+++ scripts/telekinetic_path_platform.gd
@@ -1,10 +1,9 @@
 extends Path2D
 
 @onready var animBody = $AnimatableBody2D
-@onready var sprite = $AnimatableBody2D/Sprite2D
+@onready var line = $AnimatableBody2D/Line2D
 @onready var marker = $AnimatableBody2D/Marker
 @onready var teleController: TelekineticController = $AnimatableBody2D/TelekineticController
-@onready var line: Line2D = $Line2D
 
 # to set up:
 # instantiate this scene
@@ -22,9 +21,6 @@ func _ready() -> void:
 	teleController.addControl("Right Arrow", "Move platform along rail")
 	teleController.addControl("Left Arrow", "Move platform backwards along rail")
 	pathFollow.loop = loop
-	if curve:
-		var points: PackedVector2Array = curve.get_baked_points()
-		line.points = points
 	if disableOnStart:
 		teleController.set_enabled(false)
 		
@@ -42,7 +38,7 @@ func _physics_process(delta: float) -> void:
 
 func _on_telekinetic_controller_on_set_enabled(is_enabled: bool) -> void:
 	if is_enabled:
-		sprite.modulate = TelekineticController.spriteModulationColor
+		line.default_color = TelekineticController.spriteModulationColor
 	else:
-		sprite.modulate = Color("ffffff")
+		line.default_color = Color("ffffff")
 		
