a9c86992132f870139e2285493e8a0e3bff741ae
Arti-Dev <boomcreeper296@gmail.com>
2025-04-20 17:40:48 -0400

    Revamp lectern slightly

--- scripts/lectern.gd
+++ scripts/lectern.gd
@@ -1,7 +1,11 @@
 extends AnimatedSprite2D
 
 @onready var area: Area2D = $Area2D
+@onready var audioPlayer: AudioStreamPlayer2D = $AudioStreamPlayer2D
 var activated: bool = false
+
+var pagesSound = load("res://assets/Sounds/lectern book pages turning.mp3")
+var disconnectSound = load("res://assets/Sounds/Disconnect.wav")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +13,7 @@ func _ready() -> void:
 		play("lectern")
 	else:
 		play("lecternwithbook")
+	activated = PlayerGlobalVars.interactLectern
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,6 +25,13 @@ func _process(delta: float) -> void:
 				activate()
 
 func activate():
+	audioPlayer.stop()
+	audioPlayer.stream = pagesSound
+	audioPlayer.play()
+	await get_tree().create_timer(1).timeout
+	audioPlayer.stop()
+	audioPlayer.stream = disconnectSound
+	audioPlayer.play()
 	var camera = get_viewport().get_camera_2d()
 	if camera is CustomCamera:
 		camera.shake()
