461060b5efeaadb3dfb75a8de6872b7bf484970e
Arti-Dev <boomcreeper296@gmail.com>
2025-04-25 14:51:03 -0400

    Title screen music

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,11 +2,15 @@ extends CanvasLayer
 
 var firstLevel = preload("uid://bvv0kvxphwhqp")
 var credits = preload("uid://chn82e6ce6ssa") # credit scene
+@onready var audioPlayer = $AudioStreamPlayer
+@onready var fadeRect = $FadeRect
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	fadeRect.visible = true
+	var tween = create_tween()
+	tween.tween_property(fadeRect, "modulate", Color(1, 1, 1, 0), 1.0)
 
 
 func _on_play_button_pressed() -> void:
@@ -14,6 +18,12 @@ func _on_play_button_pressed() -> void:
 	PlayerGlobalVars.respawnPoint = Vector2.ZERO
 	PlayerGlobalVars.firstLoad = true
 	PlayerGlobalVars.musicProgress = 0
+	
+	var tween = create_tween()
+	tween.tween_property(audioPlayer, "volume_db", -80, 1.0)
+	var tween2 = create_tween()
+	tween2.tween_property(fadeRect, "modulate", Color(1, 1, 1, 0.75), 1.0)
+	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_packed(firstLevel)
 
 
